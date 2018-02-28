066e824f2d87cf5d2eefc62be79fb353b991fbde
daniel kelly <danieljkelly91@gmail.com>
2018-02-28 18:08:52 +0000

    added receiveCard method in hand class

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class HandTest {
 
     Hand hand;
+    Deck deck;
 //    Card card;
 //    Card card2;
 
@@ -15,11 +16,21 @@ public class HandTest {
     public void before(){
 //        card = new Card(SuitType.HEARTS, ValueType.EIGHT);
 //        card2 = new Card(SuitType.DIAMONDS, ValueType.FIVE);
+        deck = new Deck();
         hand = new Hand();
     }
 
     @Test
     public void testHandStartsEmpty(){
         assertEquals(0, hand.numberOfCards());
+    }
+
+    @Test
+    public void testCanAddCardToHand(){
+        deck.populateDeck();
+        deck.shuffleCards();
+        hand.receiveCard(deck);
+        assertEquals(51, deck.numberOfCards());
+        assertEquals(1, hand.numberOfCards());
     }
 }
