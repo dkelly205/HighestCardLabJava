098287bdbc9ae8f8c25ca6db32c6a436204e8d39
daniel kelly <danieljkelly91@gmail.com>
2018-02-28 12:12:27 +0000

    Added test to check value of card

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -6,14 +6,22 @@ import static org.junit.Assert.assertEquals;
 public class CardTest {
 
     Card card;
+    Card card1;
 
     @Before
     public void before(){
         card = new Card(SuitType.HEARTS, ValueType.EIGHT);
+        card1 = new Card(SuitType.HEARTS, ValueType.ACE);
+
     }
 
     @Test
     public void canGetSuit(){
         assertEquals(SuitType.HEARTS, card.getSuit());
+    }
+
+    @Test
+    public void aceHasValue1(){
+        assertEquals(1, card1.getValueOfCard());
     }
 }
